21f964e2b4b0fdfdd96b8c54dd937434df2fd71d
arsros <35801128+arsros@users.noreply.github.com>
2018-01-25 13:38:03 +0200

    Update MainActivity.java
    kommentti lisätty

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,7 @@ package mobiiliohjelmointi2018.harjoitus3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//kommentti lisätty committia varten 
 public class MainActivity extends AppCompatActivity {
 
     @Override
